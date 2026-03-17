427552e84d8d13d8f97ae6dedf315056873689e3
TracedInPurple <74588450+TracedInPurple@users.noreply.github.com>
2026-03-17 22:32:59 +0100

    Im going to cry

--- source/tracedinpurple/ui/FlxScaledSliceSprite.hx
+++ source/tracedinpurple/ui/FlxScaledSliceSprite.hx
@@ -78,7 +78,7 @@ class FlxScaledSliceSprite extends FlxSliceSprite
 
 		super(scaledGraphic, scaledSliceRect, width, height);
 
-		updatedSlicedHitbox();
+		updateSlicedHitbox();
 	}
 	/**
 		Quick and Easy (Lazy) Function to stretch all Elements of the Sprite
@@ -105,7 +105,7 @@ class FlxScaledSliceSprite extends FlxSliceSprite
 		Updates the new hitbox of the sliced sprite.
 		Usually good to call after resizing the sprite.
 	**/
-	public function updatedSlicedHitbox() 
+	public function updateSlicedHitbox() 
 	{
 		updateFramePixels(); // not sure if necessary but i'll keep it on for now
 		updateHitbox();
